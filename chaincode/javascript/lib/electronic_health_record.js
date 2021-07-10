814f5716d8d5bcfa1dea2827d6d1b9e913099645
Dryan <dryanwong@gmail.com>
2021-07-10 11:45:46 +0800

    Fixes

--- chaincode/javascript/lib/electronic_health_record.js
+++ chaincode/javascript/lib/electronic_health_record.js
@@ -18,11 +18,11 @@ class ElectronicHealthRecord extends Contract {
     async createProfile(ctx, patientID, name, birthDate, address) {
         console.info('START: createProfile');
         const profile = {
-            name,
-            birthDate,
-            address,
-            authorization: {},
-            records: []
+            'name': name,
+            'birthDate': birthDate,
+            'address': address,
+            'authorization': {},
+            'records': []
         }
 
         await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile)));
@@ -32,71 +32,76 @@ class ElectronicHealthRecord extends Contract {
     async isProfileExist(ctx, patientID) {
         const profileAsBytes = await ctx.stub.getState(patientID);
         if (!profileAsBytes || profileAsBytes.length === 0) {
-            throw new Error(`${patientID} does not exist`);
+            throw new Error(`Patient ${patientID} does not exist`);
         }
         return JSON.parse(profileAsBytes.toString());
     }
 
     isAuthorized(profile, patientID, operatorID) {
         if (patientID !== operatorID && profile['authorization'][operatorID] !== true){
-            throw new Error(`${operatorID} is not authorized`);
+            throw new Error(`Operator ${operatorID} is not authorized`);
         }
     }
 
     async getRecordByID(ctx, patientID, operatorID, recordID) {
         console.info('START: getRecordByID');
-        const profile = await self.isProfileExist(ctx, patientID);
-        self.isAuthorized(profile, patientID, operatorID);
-        if (profile['records'].length < recordID) {
-            throw new Error(`${recordID} does not exist`);
+        const profile = await this.isProfileExist(ctx, patientID);
+        this.isAuthorized(profile, patientID, operatorID);
+        if (profile['records'].length <= parseInt(recordID)) {
+            throw new Error(`Record ID ${recordID} does not exist`);
         }
         console.info('END: getRecordByID');
-        return profile['records'][recordID];
+        return profile['records'][parseInt(recordID)];
     }
 
     async getAllRecord(ctx, patientID, operatorID) {
         console.info('START: getAllRecord');
-        const profile = await self.isProfileExist(ctx, patientID);
-        self.isAuthorized(patientID, operatorID);
+        const profile = await this.isProfileExist(ctx, patientID);
+        this.isAuthorized(profile, patientID, operatorID);
         console.info('END: getAllRecord');
         return profile['records'];
     }
 
     async updateAuthorization(ctx, patientID, operatorID, authorizingID, newRight) {
         console.info('START: updateAuthorization');
-        const profile = await self.isProfileExist(ctx, patientID);
-        self.isAuthorized(profile, patientID, operatorID);
-        profile['authorization'][authorizingID] = newRight;
+        const profile = await this.isProfileExist(ctx, patientID);
+        this.isAuthorized(profile, patientID, operatorID);
+        profile['authorization'][authorizingID] = newRight == "true";
         await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile)));
         console.info('END updateAuthorization');
     }
 
     async createRecord(ctx, patientID, operatorID, recordData) {
         console.info('START: createRecord');
-        const profile = await self.isProfileExist(ctx, patientID);
-        self.isAuthorized(profile, patientID, operatorID);
-        profile['records'].push(JSON.parse(recordData));
+        const profile = await this.isProfileExist(ctx, patientID);
+        this.isAuthorized(profile, patientID, operatorID);
+        const record = {'author': operatorID, 'data': recordData};
+        profile['records'].push(record);
         await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile)));
         console.info('END createRecord');
-        return profile['records'].length;
+        return profile['records'].length - 1;
     }
 
     async updateRecordByID(ctx, patientID, operatorID, recordID, newRecordData) {
         console.info('START: updateRecordByID');
-        const profile = await self.isProfileExist(ctx, patientID);
-        if (profile['records'].length < recordID) {
-            throw new Error(`${recordID} does not exist`);
+        const profile = await this.isProfileExist(ctx, patientID);
+        if (profile['records'].length <= parseInt(recordID)) {
+            throw new Error(`Record ID ${recordID} does not exist`);
         }
-        profile.records[recordID] = JSON.parse(newRecordData);
-        await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile));)
+        if (profile['records'][parseInt(recordID)]['author'] !== operatorID) {
+            throw new Error('Only the author can update the record');
+        }
+        profile.records[parseInt(recordID)]['data'] = newRecordData;
+        await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile)));
         console.info('END: updateRecordByID');
     }
 
     async updatePersonalData(ctx, patientID, newName, newAddress) {
         console.info('START: updatePersonalData');
-        const profile = await self.isProfileExist(ctx, patientID);
+        const profile = await this.isProfileExist(ctx, patientID);
         profile.name = newName;
         profile.address = newAddress;
+        await ctx.stub.putState(patientID, Buffer.from(JSON.stringify(profile)));
         console.info('END: updatePersonalData');
     }
 }
